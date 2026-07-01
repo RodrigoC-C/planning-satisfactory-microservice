4dadea8ca376865f86b7027225f318639842fe35
RodrigoC-C <r.cerdacalapuja@uandresbello.edu>
2026-06-30 21:18:54 -0400

    Modification of class with heritage to component example ModuleTransport, and ModuleCrafter

--- models.py
+++ models.py
@@ -1,57 +1,88 @@
-class MachineBase: 
-    """ Class father for all it machine in this sistem """
-    def __init__(
-        self,
-        name,
-        length,
-        width,
-        height,
-        area,
-        ingredients
+from dataclasses import dataclass 
+
+@dataclass
+class MachineCore:
+    """ The all machine of system contain the core"""
+    def base(
+        name: str ,
+        length: int,
+        width: int,
+        heigth: int,
+        area: int,
+        ingredients: str
     ) -> None:
+        
+        name = name
+        length = length
+        width = width
+        heigth = heigth 
+        area = area 
+        ingredients = ingredients
 
-        self.name = name
-        self.lenght = length
-        self.width = width
-        self.heigth = heigth
-        self.area = area
-        self.ingredients = ingredients
+@dataclass
+class ModuleExtracion: 
+    """ Class for machine that extraction resource example Miner, wather, oli """
+    def extract(
+        outputs: int,
+        item_minute_outputs: int
+    ) -> None: 
+        
+        outputs = outputs
+        item_minute_outputs = item_minute_outputs
 
-
-class MachineProduction(MachineBase):
-    """ Machine Production base on factory """
-    def __init__(
-        self,
-        power_usage,
-        inputs,
-        outputs,
-        overclocker_able
+@dataclass 
+class ModuleCrafter:
+    """ Module for all machine that crafter """
+    def crafter(
+        inputs: int,
+        item_minute_inputs: int,
+        outputs: int,
+        item_minute_outputs: int
     ) -> None:
+        
+        inputs = inputs
+        item_minute_inputs = item_minute_inputs
+        outputs = outputs
+        item_minute_outputs = item_minute_outputs
 
-        super().__init__(
-            name,
-            length,
-            width,
-            heigth,
-            area,
-            ingredients
-        )
-
-        self.power_usage = power_usage
-        self.inputs = inputs
-        self.outputs = outputs
-        self.overclocker_able = overclocker_able
-
-class MachineLogistic(MachineBase):
-    """ This class is indispensable? """
-    def __init__(
-        self,
-        item_min
+@dataclass
+class ModuleEnergy:
+    """ Module for all machine that usage energy """
+    def energy(
+        power_usage: int,
     ) -> None:
+        
+        power_usage = power_usage
 
-        super().__init__(
-            name
-        )
-        self.item_min = item_min
+@dataclass
+class ModuleInventory:
+    """ Module for machine that they have inventory """
+    def inventory(
+        inventory_size: int,
+        inputs: int,
+        outputs: int,
+    ) -> None:
+        
+        inventory_size = inventory_size
+        inputs = inputs
+        outputs = outputs
 
+@dataclass
+class ModuleOverclock:
+    """ Module of machine that has it enabled the overclock """
+    def overclock(
+        overclock_able: bool,
+        porcent_boost: float
+    ) -> None:
+        
+        overclock_able = overclock_able
+        porcent_boost = porcent_boost
 
+@dataclass 
+class ModuleTransport:
+    """ Module the machine of transport of item """
+    def transport(
+        item_minute = int,
+    ) -> None:
+        
+        item_minute = item_minute
